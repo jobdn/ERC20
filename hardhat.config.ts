4a0cd2e5a79ff19886c0899f58f317c4474aa008
Danila Pisarev <jobdan@bk.ru>
2022-02-23 10:15:03 +0300

    Added the mint task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { task } from "hardhat/config";
 import "hardhat-gas-reporter";
 import "@nomiclabs/hardhat-waffle";
 import "solidity-coverage";
+import { Contract, Signer } from "ethers";
 
 task("accounts", "Prints the list of accounts", async (args, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -10,6 +11,29 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
     console.log(await account.address);
   }
 });
+
+task("mint", "Mint tokens to address")
+  .addParam("owner", "Owner of tokens")
+  .addParam("amount", "Amount of tokens")
+  .setAction(async (taskArgs, hre, runSuper) => {
+    const owner: Signer = (await hre.ethers.getSigners())[0];
+    const ERC20Address: string = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+
+    const ERC20Contract: Contract = await hre.ethers.getContractAt(
+      "ERC20",
+      ERC20Address
+    );
+    const ERCWithSigner = ERC20Contract.connect(owner);
+
+    await ERCWithSigner.mint(taskArgs.owner, +taskArgs.amount);
+    const totalSupply = await ERC20Contract.totalSupply();
+    console.log("TOTAL SUPPLY: ", totalSupply);
+  });
+
+task("transfer", "Transfer tokens to address")
+  .addParam("to", "The account address where tokens are sended")
+  .addParam("amount", "Amount of tokens")
+  .setAction(async (taskArgs, hre, runSuper) => {});
 
 export default {
   solidity: "0.8.4",
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,6 +5,7 @@ async function deploy() {
   const ERC20 = await ERC20Factory.deploy();
   await ERC20.deployed();
 
+  process.env.ERC20_ADDRESS = ERC20.address;
   console.log("ERC20 constract is deployed to: ", ERC20.address);
 }
 
