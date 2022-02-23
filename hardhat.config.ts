8012118d52aad9e40370437ccada77004371dbfd
Danila Pisarev <jobdan@bk.ru>
2022-02-23 15:02:19 +0300

    The working version of mint task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,10 @@ import { task } from "hardhat/config";
 import "hardhat-gas-reporter";
 import "@nomiclabs/hardhat-waffle";
 import "solidity-coverage";
-import { Contract, Signer } from "ethers";
+import { BigNumber, Contract, Signer } from "ethers";
+import { config } from "dotenv";
+import { resolve } from "path";
+config({ path: resolve(__dirname, "./.env") });
 
 task("accounts", "Prints the list of accounts", async (args, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -16,18 +19,19 @@ task("mint", "Mint tokens to address")
   .addParam("owner", "Owner of tokens")
   .addParam("amount", "Amount of tokens")
   .setAction(async (taskArgs, hre, runSuper) => {
-    const owner: Signer = (await hre.ethers.getSigners())[0];
-    const ERC20Address: string = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+    const owner: Signer = (await hre.ethers.getSigners())[1];
+    const ERC20Address: string = process.env.ERC_ADDRESS?.toString() as string;
 
     const ERC20Contract: Contract = await hre.ethers.getContractAt(
       "ERC20",
-      ERC20Address
+      // TODO: adding the address
+      "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
     );
     const ERCWithSigner = ERC20Contract.connect(owner);
 
     await ERCWithSigner.mint(taskArgs.owner, +taskArgs.amount);
-    const totalSupply = await ERC20Contract.totalSupply();
-    console.log("TOTAL SUPPLY: ", totalSupply);
+    const totalSupply: BigNumber = await ERCWithSigner.totalSupply();
+    console.log("TOTAL SUPPLY: ", totalSupply.toNumber());
   });
 
 task("transfer", "Transfer tokens to address")
@@ -37,4 +41,10 @@ task("transfer", "Transfer tokens to address")
 
 export default {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_PRIVATE_KEY}`,
+      accounts: [process.env.RINKEBY_PRIVATE_KEY],
+    },
+  },
 };
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,7 +5,6 @@ async function deploy() {
   const ERC20 = await ERC20Factory.deploy();
   await ERC20.deployed();
 
-  process.env.ERC20_ADDRESS = ERC20.address;
   console.log("ERC20 constract is deployed to: ", ERC20.address);
 }
 
