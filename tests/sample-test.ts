aa6c3ad45e4f788c15de262e14242e86486d2d96
Danila Pisarev <jobdan@bk.ru>
2022-02-24 07:56:53 +0300

    Added the cheching for the transfer method

--- tests/sample-test.ts
+++ tests/sample-test.ts
@@ -1,18 +1,25 @@
 import { ethers } from "hardhat";
 import { expect } from "chai";
-import { Contract } from "ethers";
+import { ERC20__factory, ERC20 } from "../typechain-types";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 describe("ERC20", function () {
-  let ERC20: Contract;
+  let ERC20: ERC20;
+  let owner: SignerWithAddress,
+    acc1: SignerWithAddress,
+    acc2: SignerWithAddress;
+
   beforeEach(async () => {
-    const ERC20Factory = await ethers.getContractFactory("ERC20");
+    [owner, acc1, acc2] = await ethers.getSigners();
+    const ERC20Factory: ERC20__factory = (await ethers.getContractFactory(
+      "ERC20"
+    )) as ERC20__factory;
     ERC20 = await ERC20Factory.deploy();
     await ERC20.deployed();
   });
 
   describe("Mint tokens", () => {
     it("Should mint tokens", async () => {
-      const [owner] = await ethers.getSigners();
       ERC20.mint(owner.address, 1000);
       ERC20.mint(owner.address, 1000);
       expect(await ERC20.balanceOf(owner.address)).to.equal(2000);
@@ -23,6 +30,32 @@ describe("ERC20", function () {
       await expect(
         ERC20.mint(ethers.constants.AddressZero, 1000)
       ).to.be.revertedWith("Zero address");
+    });
+  });
+
+  describe("Transactions with transfer", () => {
+    it("Should transfer tokens between accounts", async () => {
+      await ERC20.mint(owner.address, 1000);
+      expect(await ERC20.balanceOf(owner.address)).to.equal(1000);
+
+      // Send 0 tokens
+      await ERC20.transfer(acc1.address, 0);
+      expect(await ERC20.balanceOf(owner.address)).to.equal(1000);
+
+      // Send 500 tokens
+      await ERC20.transfer(acc1.address, 500);
+      expect(await ERC20.balanceOf(owner.address)).to.equal(500);
+      expect(await ERC20.balanceOf(owner.address)).to.equal(500);
+    });
+
+    it("Should fail if transfer doen't have anough tokens", async () => {
+      await ERC20.mint(owner.address, 1000);
+      expect(await ERC20.balanceOf(owner.address)).to.equal(1000);
+
+      await expect(ERC20.transfer(acc1.address, 2000)).to.be.revertedWith(
+        "Not enough tokens"
+      );
+      expect(await ERC20.balanceOf(owner.address)).to.equal(1000);
     });
   });
 });
